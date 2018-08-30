18eb0ce83aa841526dc0d5ab1e1cf0d7d49646b8
Stan <nedbailov375426@gmail.com>
2018-08-30 13:00:39 -0400

    Create user without stripe

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const path = require('path');
 const Hapi = require('hapi');
 const querystring = require('querystring');
 const env = require('../.env.bell');
-const fetch = require('node-fetch');
-const FormData = require('form-data');
-// const stripe = require('stripe')(env.key);
+const requestMod = require('request');
+const script = require('./script');
 
 const server = Hapi.server({
     port: 3000,
@@ -20,20 +19,6 @@ const server = Hapi.server({
 
 
 const init = async () => {
-    // await server.register([
-    //     bell
-    // ]);
-    // server.auth.strategy('stripe', 'bell', {
-    //     provider     : 'stripe',
-    //     ttl          : 60 * 60 * 24,
-    //     password     : env.key,
-    //     clientId     : env.id,
-    //     clientSecret : env.secret,
-    //     scope        : ['read_write'],
-    //     isHttpOnly   : true,
-    //     isSecure     : true,
-    //     forceHttps   : true
-    // });
     server.route([{
         method: 'GET',
         path: '/',
@@ -47,35 +32,35 @@ const init = async () => {
         path   : '/connect-artist',
         async handler(request, h) {
             if (request.query.code) {
-                console.log('code', request.query.code);
-                const form = new FormData();
-                form.append('grant_type', 'authorization_code');
-                // form.append('client_id', env.id);
-                // form.append('client_secret', env.secret);
-                form.append('code', request.query.code);
-                try {
-                    const post = await fetch(`https://connect.stripe.com/oauth/token?${querystring.stringify({ client_id: env.id, client_secret: env.secret})}`, {method: 'POST', body: form, headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }})
-                    console.log('[post]', post);
-                    return post;
-                } catch(err) {
-                    console.log('[err]', err);
-                }
+
+                requestMod.post('https://connect.stripe.com/oauth/token', {
+                    form: {
+                        'grant_type' : 'authorization_code',
+                        'client_id' : env.id,
+                        'client_secret' : env.secret,
+                        'code' : request.query.code
+                    },
+                    json: true
+                  }, (err, response, body) => {
+                    if (err || body.error) {
+                      console.log('The Stripe onboarding process has not succeeded.');
+                    } else {
+                        console.log('[body]', body)
+                    }
+                  });
             }
             else {
                 const parameters = {
                     client_id: env.id,
                     redirect_uri: 'https://localhost:3000/connect-artist'
                 };
-                console.log('[paramentrs]', querystring.stringify(parameters));
                 return h.redirect('https://connect.stripe.com/express/oauth/authorize' + '?' + querystring.stringify(parameters));
             }
         }
     }]);
     await server.start();
-    console.log('[env]', env);
     console.log(`Server running at: ${server.info.uri}`);
+    script();
 };
 
 process.on('unhandledRejection', (err) => {
