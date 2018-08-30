86b69a15807b388f520aa546c126e466669bca2a
Stan <nedbailov375426@gmail.com>
2018-08-30 11:11:04 -0400

    No bell

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Hapi = require('hapi');
-const bell = require('bell');
+const querystring = require('querystring');
 const env = require('../.env.bell');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+// const stripe = require('stripe')(env.key);
 
 const server = Hapi.server({
     port: 3000,
@@ -17,20 +20,20 @@ const server = Hapi.server({
 
 
 const init = async () => {
-    await server.register([
-        bell
-    ]);
-    server.auth.strategy('stripe', 'bell', {
-        provider     : 'stripe',
-        ttl          : 60 * 60 * 24,
-        password     : env.key,
-        clientId     : env.id,
-        clientSecret : env.secret,
-        scope        : ['read_write'],
-        isHttpOnly   : true,
-        isSecure     : true,
-        forceHttps   : true
-    });
+    // await server.register([
+    //     bell
+    // ]);
+    // server.auth.strategy('stripe', 'bell', {
+    //     provider     : 'stripe',
+    //     ttl          : 60 * 60 * 24,
+    //     password     : env.key,
+    //     clientId     : env.id,
+    //     clientSecret : env.secret,
+    //     scope        : ['read_write'],
+    //     isHttpOnly   : true,
+    //     isSecure     : true,
+    //     forceHttps   : true
+    // });
     server.route([{
         method: 'GET',
         path: '/',
@@ -42,18 +45,36 @@ const init = async () => {
     {
         method : 'GET',
         path   : '/connect-artist',
-        config : {
-            auth : {
-                strategy : 'stripe',
-                mode     : 'required'
+        async handler(request, h) {
+            if (request.query.code) {
+                console.log('code', request.query.code);
+                const form = new FormData();
+                form.append('grant_type', 'authorization_code');
+                // form.append('client_id', env.id);
+                // form.append('client_secret', env.secret);
+                form.append('code', request.query.code);
+                try {
+                    const post = await fetch(`https://connect.stripe.com/oauth/token?${querystring.stringify({ client_id: env.id, client_secret: env.secret})}`, {method: 'POST', body: form, headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }})
+                    console.log('[post]', post);
+                    return post;
+                } catch(err) {
+                    console.log('[err]', err);
+                }
             }
-        },
-        handler(request, h) {
-            console.log('[request]');
-            return h.redirect('/');
+            else {
+                const parameters = {
+                    client_id: env.id,
+                    redirect_uri: 'https://localhost:3000/connect-artist'
+                };
+                console.log('[paramentrs]', querystring.stringify(parameters));
+                return h.redirect('https://connect.stripe.com/express/oauth/authorize' + '?' + querystring.stringify(parameters));
+            }
         }
     }]);
     await server.start();
+    console.log('[env]', env);
     console.log(`Server running at: ${server.info.uri}`);
 };
 
